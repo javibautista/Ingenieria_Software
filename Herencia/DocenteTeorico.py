f2c1c6277b6966d3bba1ce038ebb29753e6e5456
Javier Bautista <javibautista333@gmail.com>
2019-06-09 23:04:06 -0300

    2ª commit Clase DocenteTeorico agrego [] para materia[lista]y acento

--- Herencia/DocenteTeorico.py
+++ Herencia/DocenteTeorico.py
@@ -5,9 +5,9 @@ class DocenteTeorico(Persona, Docente):
 
     def dar_teorico(self, unidad):
         return unidad
-    #materia = ['Geografia', 'Matemática', 'Programación']
+    #materia = ['Geografía', 'Matemática', 'Programación']
     def proponer_examen(self):
-        if self.materia[0] == 'Geografia':
+        if self.materia[0] == 'Geografía':
             print('Cuál es el río más ancho del mundo?\n1) De la plata\n2) Nylo\n3) Amazonas')
         elif self.materia[1] == 'Matemática':
             print('Cuántos lados tiene un polígono?\n1) 5\n2) Más de 2\n3) 5 o más')
