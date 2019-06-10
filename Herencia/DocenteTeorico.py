de8cc41eaab27c936fb05695821a1ff4786b36cb
Javier Bautista <javibautista333@gmail.com>
2019-06-09 22:54:30 -0300

    2ª commit Clase DocenteTeorico agrego [] para materia[lista]

--- Herencia/DocenteTeorico.py
+++ Herencia/DocenteTeorico.py
@@ -5,11 +5,11 @@ class DocenteTeorico(Persona, Docente):
 
     def dar_teorico(self, unidad):
         return unidad
-
+    #materia = ['Geografia', 'Matemática', 'Programación']
     def proponer_examen(self):
-        if self.materia == 'Geografia':
+        if self.materia[0] == 'Geografia':
             print('Cuál es el río más ancho del mundo?\n1) De la plata\n2) Nylo\n3) Amazonas')
-        elif self.materia == 'Matemática':
+        elif self.materia[1] == 'Matemática':
             print('Cuántos lados tiene un polígono?\n1) 5\n2) Más de 2\n3) 5 o más')
-        elif self.materia == 'Programación':
+        elif self.materia[2] == 'Programación':
             print('Que es Scrum?\n1) Una forma de escribir código\n2) Una metodología ágil\n3) Una herramienta de debugging')
