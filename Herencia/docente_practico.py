fe6e4b629571d1d8e83d897331e83bf54976476f
jonatan <yoniavila19@gmail.com>
2019-06-13 21:24:21 -0300

    subo mi primer archivo

---
+++ Herencia/docente_practico.py
@@ -0,0 +1,11 @@
+class DocentePractico(Docente):
+     def __init__(self,materia)
+         self.materia=materia
+
+     def DarPractico(self,unidad):
+        self.unidad=unidad
+        if materia[1]== 'Matematica':
+        	print ("cuanta plata gastaste si hiciste 10km en un auto que consume 1.5litros por cada kilometro y la nafta cuesta 50 pesos por litro? \n1)800,\n2)750,\n3)500" )
+	else:
+             if materia[2]== 'Programacion':
+                print ("En python,que valor resulta de hacer 'a==b==a is b' si tanto a como b tiene el valor [1,2,3]? \n1)True,\n2)False,\n3)Da error")
