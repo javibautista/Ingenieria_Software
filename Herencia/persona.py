990b66fbb8240e1ce5ca7d93028add3916f9fa6d
Javier Bautista <javibautista333@gmail.com>
2019-07-01 14:35:11 -0300

    implementacion de **kw

--- Herencia/persona.py
+++ Herencia/persona.py
@@ -7,6 +7,7 @@ class Persona:
 		
 	def habla(self, string):
 		print(string)
+	#Me permite saber si lo que pregunto ocupa la misma direccionde memoria
 	def __cmp__(self, otro):
 		diferencia = (self.edad) - (otro.edad)
 		if diferencia < 0:
@@ -15,37 +16,33 @@ class Persona:
 			return 1
 		else:
 			return 0
+	#Comparo 2 objetos para saber si son iguales
 	def __eq__(self,objeto2):
-		print('Las dades son iguales?:',end='')
+		print('Las edades son iguales?:',end='')
 		if self.edad==objeto2.edad:
 			return True
 		else:
 			return False
 	#def __eq__(self, other):
 		#return ((self.apellido, self.nombre) == (other.apellido, other.nombre))
-
+	#me devuelve la representacion del objeto
 	def __repr__(self):
-		return "%s %s" % (self.nombre, self.apellido)
+		#return self.nombre, self.apellido  #-->('Juan', 'Ramirez')
+		return "%s %s" % (self.nombre, self.apellido)   #--> Juan Ramirez
 		
 class Docente(Persona):
-	def __init__(self, nombre, apellido, dni, edad, materia, numero_de_legajo, cantidad_de_alumnos_a_cargo, anios_de_antiguedad):
-		#Persona.__init__(self, nombre, apellido, dni, edad)
-		super().__init__(nombre, apellido, dni, edad)
+	def __init__(self, materia, numero_de_legajo, cantidad_de_alumnos_a_cargo, anios_de_antiguedad, **kw):#**kw es llamado si es requerido
+		#--> **:comprime,guarda(diccionario)
+		#--> kw: variable toma 1ª arg posicional y luego clave=valor(nombre=valor)
+		super().__init__(**kw)
 		self.materia = materia#['Geografía', 'Matemática', 'Programación']#
 		self.numero_de_legajo = numero_de_legajo
 		self.cantidad_de_alumnos_a_cargo = cantidad_de_alumnos_a_cargo
 		self.anios_de_antiguedad = anios_de_antiguedad
 
 	def __str__(self):
-		return "Nombre: {}, Apellido: {}, DNI: {}, Edad: {}, respuesta del alumno:{}".format(
-		self.nombre, self.apellido, self.dni, self.edad, self.respuesta_alumno)
-
-	#def corregir(self, respuesta):
-		#respuesta = respuesta#[(respuesta_correcta), (respuesta_alumno)]
-		#promedio1 = (respuesta[1][0] * 100) / respuesta[0][0] #comparo las tuplas
-		#promedio2 = (respuesta[1][1] * 100) / respuesta[0][1]
-		#promedio = (promedio1 + promedio2) / 2
-		#print('El promedio del alumno es del: {} %'.format(promedio))
+		return "materia:{}, numero de legajo:{}, cantidad de alumnos a cargo:{}, años de antiguedad:{}".format(
+		 self.materia, self.numero_de_legajo, self.cantidad_de_alumnos_a_cargo, self.anios_de_antiguedad)
 
 	def corregir(self, respuesta_correcta, respuesta_alumno):
 		self.respuesta_correcta = respuesta_correcta
@@ -65,14 +62,12 @@ class Docente(Persona):
 
 
 class DocenteTeorico(Docente):
-	def __init__(self, nombre, apellido, dni, edad, materia, numero_de_legajo, cantidad_de_alumnos_a_cargo, anios_de_antiguedad):
-		#Persona.__init__(self, nombre, apellido, dni, edad)
-		super().__init__(nombre, apellido, dni, edad, materia, numero_de_legajo, cantidad_de_alumnos_a_cargo, anios_de_antiguedad)
+	def __init__(self, **kw):
+		super().__init__( **kw)
 
 	def dar_teorico(self, unidad):
 		self.unidad = unidad
 		return 'El docente teorico da la: {}'.format(self.unidad)
-		#materia = ['Geografía', 'Matemática', 'Programación']
 	def proponer_examen(self):
 		if self.materia == 'Geografía':
 			print('Cuál es el río más ancho del mundo?\n1) De la plata*\n2) Nylo\n3) Amazonas')
@@ -82,9 +77,9 @@ class DocenteTeorico(Docente):
 			print('Que es Scrum?\n1) Una forma de escribir código\n2) Una metodología ágil*\n3) Una herramienta de debugging')
 
 class DocentePractico(Docente):
-	def __init__(self, nombre, apellido, dni, edad, materia, numero_de_legajo, cantidad_de_alumnos_a_cargo, anios_de_antiguedad):
-		#Persona.__init__(self, nombre, apellido, dni, edad)
-		super().__init__(nombre, apellido, dni, edad, materia, numero_de_legajo, cantidad_de_alumnos_a_cargo, anios_de_antiguedad)
+
+	def __init__(self, **kw):
+		super().__init__(**kw)
 
 	def dar_practico(self,unidad):
 		self.unidad = unidad
@@ -102,28 +97,39 @@ if __name__ == '__main__':
 	docente.dar_clases('Unidad Nª 5')
 	docente.corregir([(10,20),(5,10)])
 	"""
-	print('______ TEORICO ______')
-	teo = DocenteTeorico('Juan', 'Ramirez', 23465762,33, 'Geografía',35462, 12, 3)
-	print(teo.dar_teorico('Unidad Nª8'))
-	teo.proponer_examen()
-	teo.corregir((10,20),(5,10))
-	#
-	teo.habla('Hola')
-	print(str(teo))
-	print('______ PRACTICO ______')
-	pra = DocentePractico('Carla', 'Fernandez', 22463762,21, 'Matemática',12462, 17, 4)
-	pra.dar_practico('Unidad Nª1')
-	pra.corregir((14,25),(14,10))
-	pra.habla('Soy profesora')
-	print(str(pra))
-	print(teo.__eq__(pra))
-	print(pra.__repr__())
-	print(teo.__cmp__(pra))
-	print('______ TEORICO2 ______')
-	teo2 = DocenteTeorico('Myrian', 'Sosa', 21465762,35, 'Programación',35462, 22, 5)
-	print(teo2.dar_teorico('Unidad Nª2'))
-	teo2.proponer_examen()
-	teo2.corregir((10,20),(10,20))
-	print(str(teo2))
-	print('dir(obj):\n %s \n'%(dir(teo)))
-	print('vars(obj):\n %s'%(vars(teo)))
+	print('______ DOCENTE ______')
+	doc = Docente('Geografía',numero_de_legajo=35462, cantidad_de_alumnos_a_cargo=12, anios_de_antiguedad=3,nombre='Juan', apellido='Ramirez', dni=23465762,edad=33)
+	# Redefinir el metodo str me permite printear el objeto
+	print(doc)
+	print(doc.__repr__())
+	#print('dir(obj):\n %s \n'%(dir(doc)))
+	print('vars(obj):\n %s'%(vars(doc)))
+	# Se agregó el **kw, para instanciar ahora se debe poner:
+	#el primer argumento como posicional y luego po clave(nombre igualado a su valor)
+	
+	#print('______ DOCENTE TEORICO ______')
+	#teo = DocenteTeorico(nombre='Juan', apellido='Ramirez', dni=23465762,edad=33, materia='Geografía',numero_de_legajo=35462, cantidad_de_alumnos_a_cargo=12, anios_de_antiguedad=3)
+	#print(teo.dar_teorico('Unidad Nª8'))
+	#teo.proponer_examen()
+	#teo.corregir((10,20),(5,10))
+	
+	#teo.habla('Hola')
+	#print(str(teo))
+	#print('______ DOCENTE PRACTICO ______')
+	#pra = DocentePractico('Carla', 'Fernandez', 22463762,21, 'Matemática',12462, 17, 4)
+	#pra.dar_practico('Unidad Nª1')
+	#pra.corregir((14,25),(14,10))
+	#pra.habla('Soy profesora')
+	#print(str(pra))
+	#print(teo.__eq__(pra))
+	#print(pra.__repr__())
+	#print(teo.__cmp__(pra))
+	#print('______ TEORICO2 ______')
+	#teo2 = DocenteTeorico('Myrian', 'Sosa', 21465762,35, 'Programación',35462, 22, 5)
+	#print(teo2.dar_teorico('Unidad Nª2'))
+	#teo2.proponer_examen()
+	#teo2.corregir((10,20),(10,20))
+	#print(str(teo2))
+	#print('dir(obj):\n %s \n'%(dir(teo)))
+	#print('vars(obj):\n %s'%(vars(teo)))
+	
--- Herencia/poligono.py
+++ Herencia/poligono.py
@@ -1,16 +1,15 @@
 import math
 
 class Poligono:
-    def __init__(self,vertices):
-        self.vertices = vertices#[(x,y), (x, y), (x, y)]
+    def __init__(self, vertices):
+        self.vertices = vertices
         #print(self.vertices[1][0])
     def __str__(self):
         return str(self.vertices)
 
 class Rectangulo(Poligono):
-    def __init__(self,vertices):#, vertices):
-        #Poligono.__init__(self,vertices)
-        super().__init__(vertices)
+    def __init__(self, **kw):
+        super().__init__(**kw)
     def altura(self):
         if self.vertices[0][0] == self.vertices[1][0]:
             altura = math.sqrt (abs((self.vertices[1][0]- self.vertices[0][0])**2+(self.vertices[1][1]- self.vertices[0][1])**2))
@@ -42,14 +41,12 @@ class Rectangulo(Poligono):
 if __name__ == '__main__':
     print('_____________clase Rectangulo_______________')
     print()
-    pol = Poligono([(1,2),(1,5),(6,2),(6,5)])
+    #pol = Poligono([(1,2),(1,5),(6,2),(6,5)])
     #rect = Rectangulo([[1,2],[1,5],[6,2],[6,5]])
-    rect = Rectangulo([(1,2),(1,5),(6,2),(6,5)])
+    rect = Rectangulo(vertices = [(1,2),(1,5),(6,2),(6,5)])
     print(rect.altura())
     print(rect.base())
     print(rect.superficie())
-    #[[1,2], [3,4]]
-    #a[0][0]
-    print(str(pol))
-    print(dir(pol))
-    print(vars(pol))
+    print(str(rect))
+    print(dir(rect))
+    print(vars(rect))
