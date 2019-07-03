44009c19e66d6e610d7b074b1ac4e1461a76a268
myriansosa <myriansosa-1972@hotmail.com>
2019-07-03 17:49:35 -0300

    cambio de nombre y metodo corregir

--- Herencia/persona.py
+++ Herencia/persona.py
@@ -1,39 +1,64 @@
-
-class Persona:
-	def __init__(self, nombre, apellido, dni, edad):
+class Persona: # Creamos la clase padre
+	def __init__(self, nombre, apellido, dni, edad): # definimos sus atributos 
 		self.nombre = nombre
 		self.apellido = apellido
 		self.dni = dni
 		self.edad = edad
-		
-	def habla(self, string):
-		print(string)
-		
-class Docente(Persona):
-	def __init__(self, nombre, apellido, dni, edad, materia, numero_de_legajo,
-	 cantidad_de_alumnos_a_cargo, anios_de_antiguedad):
-		 Persona.__init__(self, nombre, apellido, dni, edad)
-		 self.materia = ["Geografia", "Matematica", "Programacion"]
-		 self.numero_de_legajo = numero_de_legajo
-		 self.cantidad_de_alumnos_a_cargo = cantidad_de_alumnos_a_cargo
-		 self.anios_de_antiguedad = anios_de_antiguedad
-		 
-	def corregir(self, respuestas=[(1,2),(1,3)]):	
-		respuesta_correcta = respuestas[0]
-		respuesta_alumno = respuestas[1]
-		promedio = respuesta_correcta == respuesta_alumno #comparo las tuplas
-		print (promedio)
-			
-	def dar_clases(self):#Imprimo por pantalla que materia da y cantidad de alumnos  
-		unidad = 'Hola soy profesor de  %s y tengo %s alumnos' % (self.materia[0], self.cantidad_de_alumnos_a_cargo)
-		print (unidad)
-		
+	
+	def __str__(self):
+		""" Devuelve una cadena representativa de persona """
+		cadena = self.nombre +","+ self.apellido +","+ str(self.dni) +","+ str(self.edad)
+		return cadena
+	
+	def habla(self, string): # Metodo de instancia
+		print (string)
 
-				
+	def __cmp__(self, otro):
+		diferencia = (self.edad) - (otro.edad)
+		if diferencia < 0:
+			return -1
+		elif diferencia > 0:
+			return 1
+		else: 
+			return 0
+		
+class Docente(Persona): # Creamos la clase hija que hereda de padre
+	def __init__(self, materia, numero_de_legajo,
+		cantidad_de_alumnos_a_cargo, anios_de_antiguedad, **kw):
+		self.materia = materia  # Especificamos el nuevo atribuo de la clase hija
+		self.numero_de_legajo = numero_de_legajo
+		self.cantidad_de_alumnos_a_cargo = cantidad_de_alumnos_a_cargo
+		self.anios_de_antiguedad = anios_de_antiguedad
+		super().__init__(**kw)
+		
+	def __str__(self):
+		caden = self.nombre +","+ self.apellido +","+ str(self.dni) +","+ str(self.edad) 
+		","+ self.materia +","+ str(self.numero_de_legajo) +","+ str(self.cantidad_de_alumnos_a_cargo) 
+		","+ str(self.anios_de_antiguedad)
+		return caden
+		 
+	def corregir(self, lista):
+		contador = 0
+		for respuesta in lista:
+			if respuesta[0] == respuesta[1]:
+				contador += 1
+		promedio = contador / len(lista)
+		print (promedio)
+
+	def dar_clases(self): #Imprimo por pantalla que materia da y cantidad de alumnos  
+		mensaje = 'Hola soy profesor de  %s y tengo %s alumnos' % (self.materia,
+			self.cantidad_de_alumnos_a_cargo)
+		print (mensaje)
+
+
 if __name__ == '__main__':
-	profe = Persona('Martin', 'Gaiten', 25678930, 38)
-	docente = Docente("maria", "uarez", 23465762,33, "matematicas",35462, 12, 3)
-	docente.dar_clases()
-	docente.corregir()
-	
-	
+	pers = Persona("jose","suarez", 24567822, 44)
+	pers.habla("buenas tardes alumnado")
+	print(pers.nombre)
+	print(str(pers))
+	doc = Docente("Geografia", 435576, 12,3, nombre = "Fernando", apellido = "Suares", dni = 34567890,edad = 33)
+	print(str(doc))
+	print(doc.materia)
+	print(doc.nombre)
+	doc.dar_clases()
+	print(doc.habla())
