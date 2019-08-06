c51877d46b5403e6ab34407abc947e61cb1b421c
Javier Bautista <javibautista333@gmail.com>
2019-08-06 17:22:15 -0300

    Agregando 2º decorador

--- vehiculo_dec.py
+++ vehiculo_dec.py
@@ -11,16 +11,23 @@ class Vehiculo:
 def verificar_ruedas(f):
     def nuevafuncion(self, *args, **kwargs):
         f(self, *args, **kwargs)
-        if self.posicion%100 == 0:
-            print('Verificar ruedas, el {} tiene 100km recorridos'.format(type(self).__name__))
+        if type(self).__name__ == 'Auto' and self.posicion%100 == 0:
+            print('Verificar ruedas, el {} superó los 100km recorridos'.format(type(self).__name__))
+        elif type(self).__name__ == 'Camion' and self.posicion%20 == 0:
+            print('Verificar ruedas, el {} superó los 20km recorridos'.format(type(self).__name__))
+        else:
+            pass
     return nuevafuncion
-
+'''
 def verificar_ruedas_camion(f):
     def nuevafuncion(self, *args, **kwargs):
         f(self, *args, **kwargs)
         if self.posicion%20 == 0:
-            print('Verificar ruedas, el {} tiene 20km recorridos'.format(type(self).__name__))
-    return nuevafuncion
+            print('Verificar ruedas, el {} superó los 20km recorridos'.format(type(self).__name__))
+    return nuevafuncion'''
+
+def carga_limite(f):
+    def nueva
 
 class Auto(Vehiculo):
     def __init__(self, **kw):
@@ -29,16 +36,25 @@ class Auto(Vehiculo):
     @verificar_ruedas
     def andar(self):
         self.posicion += 20
+        #print(self.posicion)
         return self.posicion
 
 class Camion(Vehiculo):
     def __init__(self, **kw):
         super().__init__(**kw)
+        self.carga_actual = 0
     
-    @verificar_ruedas_camion
+    @verificar_ruedas
     def andar(self):
         self.posicion += 5
+        #print(self.posicion)
         return self.posicion
+    
+        
+    def cargar(self, numero=0):
+        self.carga_actual += numero
+        print('{} kgs'.format(self.carga_actual))
+        return self.carga_actual
 
 if __name__ == '__main__':
     print('\n_____ AUTO _____\n')
@@ -55,3 +71,5 @@ if __name__ == '__main__':
     c.andar()
     c.andar()
     manejar_vehiculo(c)
+    c.cargar(30)
+    c.cargar(40)
