294ff2fb5774671e65d8a8f1c996da05e8e32072
javibautista <javibautista333@gmail.com>
2019-10-30 13:39:30 -0300

    indice

--- proyecto-voto/mysite/encuestas/views.py
+++ proyecto-voto/mysite/encuestas/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 
+from .models import Pregunta
+
 # index: indice, question: pregunta
 def indice(request):
-    return HttpResponse("Hola Mundo. Estás en el index(índice) de encuestas.")
+    # latest_question_list: ultima_pregunta_lista
+    ultima_pregunta_lista = Pregunta.objects.order_by('-pub_fecha')[:5]
+    # output: salida
+    salida = ', '.join([q.pregunta_text for q in ultima_pregunta_lista])
+    return HttpResponse(salida)
 
 # detail: detalle
 def detalle(request, pregunta_id):
