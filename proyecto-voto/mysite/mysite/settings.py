b867a7bf2d0efefb83313711d11e8d34d91741a5
javibautista <javibautista333@gmail.com>
2019-10-30 10:44:40 -0300

    setting, sqlite3

--- proyecto-voto/mysite/mysite/settings.py
+++ proyecto-voto/mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'encuestas.apps.EncuestasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
