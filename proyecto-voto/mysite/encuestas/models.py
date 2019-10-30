b7f953a34316545c93d392b74f6e20075fadf2ff
javibautista <javibautista333@gmail.com>
2019-10-30 12:27:19 -0300

    models, admin, settings

--- proyecto-voto/mysite/encuestas/models.py
+++ proyecto-voto/mysite/encuestas/models.py
@@ -1,3 +1,25 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
+# Question
+class Pregunta(models.Model):
+    pregunta_text = models.CharField(max_length=200)
+    pub_fecha = models.DateTimeField('fecha de publicación')
+    
+    def __str__(self):
+        return self.pregunta_text
+    
+    # was_published_recently
+    def fue_publicado_recientemente(self):
+        return self.pub_fecha >= timezone.now() - datetime.timedelta(days=1)
 
+# Choice
+class Opcion(models.Model):
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    opcion_text = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.opcion_text
