d20a2dde0b43fac8d238e1d88609cbab76bead23
javibautista <javibautista333@gmail.com>
2019-10-30 13:17:20 -0300

    indice,detalle, resultados, voto

--- proyecto-voto/mysite/encuestas/views.py
+++ proyecto-voto/mysite/encuestas/views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
 
-def index(request):
+# index: indice, question: pregunta
+def indice(request):
     return HttpResponse("Hola Mundo. Estás en el index(índice) de encuestas.")
+
+# detail: detalle
+def detalle(request, pregunta_id):
+    return HttpResponse("Estás mirando la pregunta %s." % pregunta_id)
+
+# results: resultados, response: respuesta
+def resultados(request, pregunta_id):
+    respuesta = "Estás viendo los resultados de la pregunta %s."
+    return HttpResponse(respuesta % pregunta_id)
+
+# vote: voto
+def voto(request, pregunta_id):
+    return HttpResponse("Estás votando una pregunta %s." % pregunta_id)
+
+
