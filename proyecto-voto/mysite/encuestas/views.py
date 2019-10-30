fd8b73f734d434e98e6c06c44715441a3006b78b
javibautista <javibautista333@gmail.com>
2019-10-30 14:23:23 -0300

    views change

--- proyecto-voto/mysite/encuestas/views.py
+++ proyecto-voto/mysite/encuestas/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-
-from django.template import loader
+#from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import Pregunta
 
@@ -9,17 +10,22 @@ def indice(request):
     # latest_question_list: ultima_pregunta_lista
     ultima_pregunta_lista = Pregunta.objects.order_by('-pub_fecha')[:5]
     # template: plantilla
-    plantilla = loader.get_template('encuestas/indice.html')
+    #plantilla = loader.get_template('encuestas/indice.html')
     # context: context
     contexto = {
         'ultima_pregunta_lista': ultima_pregunta_lista,
     }
     # return HttpResponse(template.render(context, request))
-    return HttpResponse(plantilla.render(contexto, request))
+    #return HttpResponse(plantilla.render(contexto, request))
+    return render(request, 'encuestas/indice.html', contexto)
 
 # detail: detalle
 def detalle(request, pregunta_id):
-    return HttpResponse("Estás mirando la pregunta %s." % pregunta_id)
+    try:
+        pregunta = Pregunta.objects.get(pk=pregunta_id)
+    except Pregunta.DoesNotExist:
+        raise Http404("La Pregunta no existe")
+    return render(request, 'encuestas/detalle.html', {'pregunta': pregunta})
 
 # results: resultados, response: respuesta
 def resultados(request, pregunta_id):
